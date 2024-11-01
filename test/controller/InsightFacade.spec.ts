709f3926db9e4b58eb356770f361f85726f23210
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-01 15:39:04 -0700

    new tessssst

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -463,6 +463,7 @@ describe("InsightFacade", function () {
 		it("[valid/c2/validRoom.json] Query valid room", checkQuery);
 		it("[valid/c2/validRoomAll.json] Query valid room all", checkQuery);
 		it("[valid/c2/transformationMultipleColumns.json] Transformation multiple columns", checkQuery);
+		it("[valid/c2/preserveNumber.json] preserve number", checkQuery);
 
 		it("[invalid/c2/invalidReferenceMultipleDataset.json] Invalid RMD", checkQuery);
 		it("[invalid/c2/invalidDuplicateApply.json] Invalid duplicate apply", checkQuery);
