bea686f4282d12d8dc2cdb45092ec00e804c8c91
angelafeliciaa <angelafc@students.cs.ubc.ca>
2024-11-18 11:44:31 -0800

    tests

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -1,47 +1,140 @@
+// got from llm
 import { expect } from "chai";
-import request, { Response } from "supertest";
+import request from "supertest";
 import { StatusCodes } from "http-status-codes";
 import Log from "@ubccpsc310/folder-test/build/Log";
+import Server from "../../src/rest/Server";
+import fs from "fs-extra";
+import path from "path";
 
 describe("Facade C3", function () {
-	before(function () {
-		// TODO: start server here once and handle errors properly
+	let server: Server;
+	const SERVER_URL = "http://localhost:4321";
+	const SERVER_PORT = 4321;
+
+	before(async function () {
+		server = new Server(SERVER_PORT);
+		try {
+			await server.start();
+			Log.info(`Server started on port ${SERVER_PORT}`);
+		} catch (err) {
+			Log.error(`Failed to start server: ${err}`);
+			expect.fail("Failed to start server");
+		}
 	});
 
-	after(function () {
-		// TODO: stop server here once!
+	after(async function () {
+		try {
+			await server.stop();
+			Log.info("Server stopped");
+		} catch (err) {
+			Log.error(`Failed to stop server: ${err}`);
+			expect.fail("Failed to stop server");
+		}
 	});
 
-	beforeEach(function () {
-		// might want to add some process logging here to keep track of what is going on
+	beforeEach(async function () {
+		// Remove the data directory to ensure a clean state
+		try {
+			await fs.remove("./data");
+			Log.info("Data directory removed");
+		} catch (err) {
+			Log.error(`Failed to remove data directory: ${err}`);
+			expect.fail("Failed to remove data directory");
+		}
 	});
 
 	afterEach(function () {
 		// might want to add some process logging here to keep track of what is going on
 	});
 
-	// Sample on how to format PUT requests
-	it("PUT test for courses dataset", function () {
-		const SERVER_URL = "TBD";
-		const ENDPOINT_URL = "TBD";
-		const ZIP_FILE_DATA = "TBD";
+	it("PUT test for sections dataset", async function () {
+		const datasetId = "courses";
+		const kind = "sections";
+		const ENDPOINT_URL = `/dataset/${datasetId}/${kind}`;
+		const zipFilePath = path.join(__dirname, "../resources/archives/courses.zip");
+		let ZIP_FILE_DATA: Buffer;
 
 		try {
-			return request(SERVER_URL)
+			ZIP_FILE_DATA = await fs.readFile(zipFilePath);
+		} catch (err) {
+			Log.error(`Failed to read ZIP file: ${err}`);
+			expect.fail("Failed to read ZIP file");
+			return; // Exit early since we can't proceed without the ZIP data
+		}
+
+		try {
+			const res = await request(SERVER_URL)
 				.put(ENDPOINT_URL)
 				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					// some logging here please!
-					expect(res.status).to.be.equal(StatusCodes.OK);
-				})
-				.catch(function () {
-					// some logging here please!
-					expect.fail();
-				});
+				.set("Content-Type", "application/x-zip-compressed");
+
+			Log.info(`Response status: ${res.status}`);
+			Log.info(`Response body: ${JSON.stringify(res.body)}`);
+			expect(res.status).to.be.equal(StatusCodes.OK);
+			expect(res.body).to.have.property("result");
+			expect(res.body.result).to.be.an("array").that.includes(datasetId);
 		} catch (err) {
-			Log.error(err);
-			// and some more logging here!
+			Log.error(`Error in PUT test: ${err}`);
+			expect.fail("PUT request failed");
+		}
+	});
+
+	it("GET test for listing datasets", async function () {
+		const ENDPOINT_URL = `/datasets`;
+
+		try {
+			const res = await request(SERVER_URL).get(ENDPOINT_URL);
+
+			Log.info(`Response status: ${res.status}`);
+			expect(res.status).to.be.equal(StatusCodes.OK);
+			expect(res.body).to.have.property("result");
+			expect(res.body.result).to.be.an("array");
+			// Add more assertions if necessary
+		} catch (err) {
+			Log.error(`Error in GET test: ${err}`);
+			expect.fail("GET request failed");
+		}
+	});
+
+	it("DELETE test for sections dataset", async function () {
+		const datasetId = "sections";
+		const ENDPOINT_URL = `/dataset/${datasetId}`;
+
+		try {
+			const res = await request(SERVER_URL).delete(ENDPOINT_URL);
+
+			Log.info(`Response status: ${res.status}`);
+			expect(res.status).to.be.equal(StatusCodes.OK);
+			expect(res.body).to.have.property("result");
+			expect(res.body.result).to.equal(datasetId);
+		} catch (err) {
+			Log.error(`Error in DELETE test: ${err}`);
+			expect.fail("DELETE request failed");
+		}
+	});
+
+	it("POST test for query", async function () {
+		const ENDPOINT_URL = `/query`;
+		const query = {
+			WHERE: {},
+			OPTIONS: {
+				COLUMNS: ["courses_dept", "courses_avg"],
+				ORDER: "courses_avg",
+			},
+		};
+
+		try {
+			const res = await request(SERVER_URL).post(ENDPOINT_URL).send(query).set("Content-Type", "application/json");
+
+			Log.info(`Response status: ${res.status}`);
+			expect(res.status).to.be.equal(StatusCodes.OK);
+			expect(res.body).to.have.property("result");
+			expect(res.body.result).to.be.an("array");
+			// Add more assertions if necessary
+		} catch (err) {
+			Log.error(`Error in POST query test: ${err}`);
+			expect.fail("POST query request failed");
 		}
 	});
 
